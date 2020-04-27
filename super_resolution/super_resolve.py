464bf77bd15d5bb670cb17c6e0caf0701cac49fa
Zack_Hsiao <eewindfly@gmail.com>
2020-04-27 14:19:53 +0800

    do preparation

--- super_resolution/super_resolve.py
+++ super_resolution/super_resolve.py
@@ -8,9 +8,14 @@ import numpy as np
 
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch Super Res Example')
-parser.add_argument('--input_image', type=str, required=True, help='input image to use')
-parser.add_argument('--model', type=str, required=True, help='model file to use')
-parser.add_argument('--output_filename', type=str, help='where to save the output image')
+parser.add_argument('--input_image', type=str,
+                    required=True, help='input image to use')
+parser.add_argument('--gt_image', type=str, default=None,
+                    help='ground truth of input image to use')
+parser.add_argument('--model', type=str, required=True,
+                    help='model file to use')
+parser.add_argument('--output_filename', type=str,
+                    help='where to save the output image')
 parser.add_argument('--cuda', action='store_true', help='use cuda')
 opt = parser.parse_args()
 
@@ -35,7 +40,11 @@ out_img_y = Image.fromarray(np.uint8(out_img_y[0]), mode='L')
 
 out_img_cb = cb.resize(out_img_y.size, Image.BICUBIC)
 out_img_cr = cr.resize(out_img_y.size, Image.BICUBIC)
-out_img = Image.merge('YCbCr', [out_img_y, out_img_cb, out_img_cr]).convert('RGB')
+out_img = Image.merge(
+    'YCbCr', [out_img_y, out_img_cb, out_img_cr]).convert('RGB')
 
 out_img.save(opt.output_filename)
 print('output image saved to ', opt.output_filename)
+
+# calculate PSNR and SSIM if ground truth image is provided
+
