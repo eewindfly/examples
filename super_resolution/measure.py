a1d4b9e606bf13cd69d797c32babcb20b884b00e
Zack_Hsiao <eewindfly@gmail.com>
2020-04-28 13:52:12 +0800

    test psnr ssim on Y channel only

--- super_resolution/measure.py
+++ super_resolution/measure.py
@@ -11,14 +11,16 @@ parser.add_argument('--gt_image', type=str, default=None,
                     help='ground truth image')
 opt = parser.parse_args()
 
-result_img = Image.open(opt.result_image)
-gt_img = Image.open(opt.gt_image)
+result_img = Image.open(opt.result_image).convert('YCbCr')
+result_img, _, _ = result_img.split()
+gt_img = Image.open(opt.gt_image).convert('YCbCr')
+gt_img, _, _ = gt_img.split()
 X = np.array(result_img)
 Y = np.array(gt_img)
 
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
     pnsr_val = compare_psnr(Y, X, data_range=255)
-    ssim_val = compare_ssim(X, Y, data_range=255, multichannel=True)
+    ssim_val = compare_ssim(X, Y, data_range=255)
 
 print(f"psnr: {pnsr_val:.2f}, ssim: {ssim_val:.4f}")
