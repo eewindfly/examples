1d3334688b423ec9cc982b0dd29158db9b062b28
Zack_Hsiao <eewindfly@gmail.com>
2020-05-25 18:20:10 +0800

    add denoising test script

---
+++ super_resolution/test_denoising.py
@@ -0,0 +1,39 @@
+import argparse
+import torch
+from PIL import Image
+from torchvision.transforms import ToTensor
+
+import numpy as np
+
+parser = argparse.ArgumentParser(description='PyTorch Denoising Example')
+parser.add_argument('--input_image', type=str,
+                    required=True, help='input image to use')
+parser.add_argument('--model', type=str, required=True,
+                    help='model file to use')
+parser.add_argument('--output_filename', type=str,
+                    help='where to save the output image')
+parser.add_argument('--cuda', action='store_true', help='use cuda')
+opt = parser.parse_args()
+
+print(opt)
+
+img = Image.open(opt.input_image)
+
+model = torch.load(opt.model)
+img_to_tensor = ToTensor()
+input = img_to_tensor(img).view(1, -1, img.size[1], img.size[0])
+
+if opt.cuda:
+    model = model.cuda()
+    input = input.cuda()
+
+out = model(input)
+out = out.cpu()
+out_img = out[0].detach().numpy()
+out_img *= 255
+out_img = out_img.clip(0, 255)
+out_img = np.transpose(out_img, (1,2,0))
+out_img = Image.fromarray(np.uint8(out_img), mode='RGB')
+
+out_img.save(opt.output_filename)
+print('output image saved to ', opt.output_filename)
