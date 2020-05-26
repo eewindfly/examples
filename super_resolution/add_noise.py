cb0916549b6ed5feeae5755df67e09ad331438fc
Zack_Hsiao <eewindfly@gmail.com>
2020-05-26 16:57:14 +0800

    add noise on Y channel only for test images

--- super_resolution/add_noise.py
+++ super_resolution/add_noise.py
@@ -14,19 +14,33 @@ parser.add_argument('--sigma',
                     help="AWGN standard deviation")
 parser.add_argument('--output_filename', type=str,
                     help='where to save the output image')
+parser.add_argument('--y_only', action='store_true',
+                    help='add noise on y channel only?')
 opt = parser.parse_args()
 
 print(opt)
 
 input_img = Image.open(opt.input_image)
 
-# add noise
-input = np.asarray(input_img)/255
-noise = np.random.normal(size=input.shape)*opt.sigma/255
-output = input + noise
-output = np.clip(output, 0, 1)
-output *= 255
-output_img = Image.fromarray(np.uint8(output), mode='RGB')
+# add noise on RGB space
+if not opt.y_only:
+    input = np.asarray(input_img)/255
+    noise = np.random.normal(size=input.shape)*opt.sigma/255
+    output = input + noise
+    output = np.clip(output, 0, 1)
+    output *= 255
+    output_img = Image.fromarray(np.uint8(output), mode='RGB')
+else:
+# add noise on Y channel only
+    input_img = input_img.convert('YCbCr')
+    y, cb, cr = input_img.split()
+    input = np.asarray(y)/255
+    noise = np.random.normal(size=input.shape)*opt.sigma/255
+    output = input + noise
+    output = np.clip(output, 0, 1)
+    output *= 255
+    output_img_y = Image.fromarray(np.uint8(output), mode='L')
+    output_img = Image.merge('YCbCr', [output_img_y, cb, cr]).convert('RGB')
 
 # save
 output_img.save(opt.output_filename)
