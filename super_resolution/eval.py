5152f1d594a8f281ddee2394b5df6393853e560b
Zack_Hsiao <eewindfly@gmail.com>
2020-06-03 17:10:04 +0800

    refactor eval

---
+++ super_resolution/eval.py
@@ -0,0 +1,54 @@
+
+from __future__ import print_function
+
+import argparse
+import glob
+import os
+
+import numpy as np
+import torch
+from PIL import Image
+
+from super_resolve import resolve
+from torchvision.transforms import ToTensor
+from pathlib import Path
+
+def get_latest_model_path_in_dir(dirpath):
+    paths = sorted(Path(dirpath).iterdir(), key=os.path.getmtime)
+
+    if len(paths) > 0:
+        return str(paths[-1])
+
+    return None
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='PyTorch Super Res Example')
+    parser.add_argument('--input_dir',
+                        type=str,
+                        required=True,
+                        help='input image dirpath')
+    parser.add_argument('--model_dir',
+                        type=str,
+                        required=True,
+                        help='model file to use')
+    parser.add_argument('--output_dir',
+                        type=str,
+                        help='where to save the output image')
+    opt = parser.parse_args()
+    print(opt)
+
+    input_image_files = glob.glob(os.path.join(opt.input_dir, "*.jpg"))
+    input_image_files += glob.glob(os.path.join(opt.input_dir, "*.png"))
+    
+    for root, dirs, files in os.walk(opt.model_dir):
+        for dirpath in dirs:
+            model_path = get_latest_model_path_in_dir(os.path.join(root, dirpath))
+            if model_path is None:
+                continue
+            
+            for input_image in input_image_files:
+                filename = os.path.basename(input_image)
+                output_image_path = os.path.join(opt.output_dir, dirpath, filename)
+                print(output_image_path)
+                os.makedirs(os.path.dirname(output_image_path), exist_ok=True)
+                resolve(model_path, input_image, output_image_path)
